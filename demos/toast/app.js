ec47d5148b2ba8ea778158d4468c0e5db5d4dcab
cy920820 <cuiyang673308817@gmail.com>
2020-01-03 16:40:12 +0800

    update demos

--- demos/toast/app.js
+++ demos/toast/app.js
@@ -1,7 +1,7 @@
 import * as uikit from '../../components'
 
 document.getElementById('btn').onclick = function () {
-  uikit.toast.show('是吗', () => {
-    alert('结束了')
+  uikit.toast.show('Toast 基本用法', () => {
+    alert('执行结束回调')
   })
 }
